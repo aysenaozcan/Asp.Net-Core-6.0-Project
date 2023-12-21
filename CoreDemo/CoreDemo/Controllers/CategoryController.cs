d49b126277d50807f04d6b433287323e2453cfc3
aysenaozcan <92749952+aysenaozcan@users.noreply.github.com>
2023-12-21 23:55:11 +0300

    .

--- CoreDemo/CoreDemo/Controllers/CategoryController.cs
+++ CoreDemo/CoreDemo/Controllers/CategoryController.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryManager cm=new CategoryManager(new EfCategoryRepository());   
         public IActionResult Index()
         {
-            return View();
+            var values = cm.GetList();
+            return View(values);
         }
     }
 }
