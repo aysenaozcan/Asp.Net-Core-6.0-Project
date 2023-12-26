f22bb24c819cd290402e92f86ffee30a722ba77a
aysenaozcan <92749952+aysenaozcan@users.noreply.github.com>
2023-12-26 17:09:19 +0300

    .

---
+++ CoreDemo/CoreDemo/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,16 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Comment
+{
+	public class CommentListByBlog:ViewComponent
+	{
+		CommentManager cm = new CommentManager(new EfCommentRepository());
+		public IViewComponentResult Invoke()
+		{
+			var values = cm.GetList(2);
+			return View(values);
+		}
+	}
+}
