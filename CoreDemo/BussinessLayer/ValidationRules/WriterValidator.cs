02a06e3725efddb745301b7d3a094121f1d60b70
aysenaozcan <92749952+aysenaozcan@users.noreply.github.com>
2023-12-28 23:03:51 +0300

    .

--- CoreDemo/BussinessLayer/ValidationRules/WriterValidator.cs
+++ CoreDemo/BussinessLayer/ValidationRules/WriterValidator.cs
@@ -13,10 +13,11 @@ namespace BussinessLayer.ValidationRules
         public WriterValidator()
         {
             RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı soyadı kısmı boş geçilemez");
-            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Yazar mail adresi kısmı boş geçilemez");
+			RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az iki karakter girişi yapın");
+			RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Lütfen en fazla elli karakter girişi yapın");
+			RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Yazar mail adresi kısmı boş geçilemez");
             RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş geçilemez");
-            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az iki karakter girişi yapın");
-            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Lütfen en fazla elli karakter girişi yapın");
+            
         }
     }
 }
