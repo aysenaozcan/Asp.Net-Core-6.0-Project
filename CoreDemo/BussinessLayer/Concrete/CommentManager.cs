104a17ff704cc6c133eed67f2aee93a7f6424cef
aysenaozcan <92749952+aysenaozcan@users.noreply.github.com>
2023-12-30 23:54:43 +0300

    .

--- CoreDemo/BussinessLayer/Concrete/CommentManager.cs
+++ CoreDemo/BussinessLayer/Concrete/CommentManager.cs
@@ -20,7 +20,7 @@ namespace BussinessLayer.Concrete
 
 		public void CommentAdd(Comment comment)
 		{
-			throw new NotImplementedException();
+			_commentdal.Insert(comment);
 		}
 
 		public List<Comment> GetList(int id)
--- CoreDemo/CoreDemo/Controllers/CommentController.cs
+++ CoreDemo/CoreDemo/Controllers/CommentController.cs
@@ -1,5 +1,6 @@
 ﻿using BussinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
@@ -11,8 +12,18 @@ namespace CoreDemo.Controllers
 		{
 			return View();
 		}
+		[HttpGet]
 		public PartialViewResult PartialAddComment()
 		{
+			return PartialView();
+		}
+		[HttpPost]
+		public PartialViewResult PartialAddComment(Comment p)
+		{
+			p.CommentDate=DateTime.Parse(DateTime.Now.ToShortDateString());	
+			p.CommentStatus=true;	
+			p.BlogId=2;
+			cm.CommentAdd(p);
 			return PartialView();
 		}
 		public PartialViewResult CommentListByBlog(int id)
