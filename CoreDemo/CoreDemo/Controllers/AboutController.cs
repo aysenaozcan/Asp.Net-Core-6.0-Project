c8914eee9ee389d48cf8346e729606df14b80912
aysenaozcan <92749952+aysenaozcan@users.noreply.github.com>
2024-01-02 00:12:15 +0300

    .

--- CoreDemo/CoreDemo/Controllers/AboutController.cs
+++ CoreDemo/CoreDemo/Controllers/AboutController.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
 	public class AboutController : Controller
 	{
+		AboutManager abm = new AboutManager(new EfAboutRepository());
 		public IActionResult Index()
 		{
-			return View();
+			var values = abm.GetList(); 
+			return View(values);
 		}
 		public PartialViewResult SocialMediaAbout()
 		{
