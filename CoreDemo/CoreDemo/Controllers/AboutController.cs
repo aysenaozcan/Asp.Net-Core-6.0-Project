9eeecc0a161b84583b3d8747a3b056ae08bf8166
aysenaozcan <92749952+aysenaozcan@users.noreply.github.com>
2024-01-01 23:50:11 +0300

    .

---
+++ CoreDemo/CoreDemo/Controllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+	public class AboutController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public PartialViewResult SocialMediaAbout()
+		{
+			return PartialView();
+		}
+	}
+}
