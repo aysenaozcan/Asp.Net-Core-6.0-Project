fd79e7063f951ad351913f7e7fd4860410ba8606
aysenaozcan <92749952+aysenaozcan@users.noreply.github.com>
2024-01-02 11:09:07 +0300

    .

---
+++ CoreDemo/CoreDemo/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+	public class ContactController : Controller
+	{
+		ContactManager cm = new ContactManager(new EfContactRepository());
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(Contact p)
+		{
+			p.ContactDate= DateTime.Parse(DateTime.Now.ToShortDateString());
+			p.ContactStatus = true;
+			cm.ContactAdd(p);
+;			return RedirectToAction("Index", "Blog");
+		}
+	}
+}
--- CoreDemo/DataAccessLayer/EntityFramework/EfContactRepository.cs
+++ CoreDemo/DataAccessLayer/EntityFramework/EfContactRepository.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.EntityFramework
 {
-    public class EfContactRepository: GenericRepository<Category>, ICategoryDal
-    {
+    public class EfContactRepository: GenericRepository<Contact>, IContactDal
+	{
     }
 }
