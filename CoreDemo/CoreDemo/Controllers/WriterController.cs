59135f5596df7e46800a97d130c279c12f24871e
aysenaozcan <92749952+aysenaozcan@users.noreply.github.com>
2024-01-02 22:57:15 +0300

    .

---
+++ CoreDemo/CoreDemo/Controllers/WriterController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+	public class WriterController : Controller
+	{
+		[Authorize]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
--- CoreDemo/CoreDemo/Program.cs
+++ CoreDemo/CoreDemo/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Serilog;
 using Serilog.Core;
 
@@ -5,8 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
 
-
+builder.Services.AddMvc(config =>
+{
+	var policy = new AuthorizationPolicyBuilder()
+					.RequireAuthenticatedUser()
+					.Build();
+	config.Filters.Add(new AuthorizeFilter(policy));
+});
 
 var app = builder.Build();
 
@@ -25,6 +34,8 @@ app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1", "?code={0}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseSession();
 
 app.UseRouting();
 
